84cd88cb7b55bb0ce3d75110a73036baaa39b561
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-09-28 16:59:43 -0300

    MotoConnectChallenge

---
+++ src/main/java/br/com/fiap/motoconnect/challenge/MotoConnectChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.motoconnect.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoConnectChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoConnectChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/motoconnect/challenge/MotoConnectChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.motoconnect.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoConnectChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
